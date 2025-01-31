47c2942a01b1fde4b884ad2859f73d806c883c86
RhexNiebres <rhexniebres@gmail.com>
2025-01-30 16:39:17 -0800

    feat: add DifficultySelector component for selecting game difficulty

---
+++ src/Components/Difficulty.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function DifficultySelector({ onDifficultyChange}){
+    const [selectedDifficulty, setSelectedDifficulty] = useState("easy");
+
+    const handleDifficultyChange = (event) =>{
+        const difficulty = event.target.value;
+        setSelectedDifficulty(difficulty);
+        onDifficultyChange(difficulty);
+    };
+    return(
+        <div>
+            <h3>Select difficulty</h3>
+            <select value={selectedDifficulty} onChange={handleDifficultyChange}>
+            <option value="easy"> Easy (6 Pokémon)</option>
+            <option value="medium"> Medium (9 Pokémon)</option>
+            <option value="hard"> Hard (12 Pokémon)</option>
+            </select>
+        </div>
+    );
+}
